46095ba81db2b1abde0765269f6c50fe7f1befa4
htringuyen <tringuyenh75@gmail.com>
2024-09-05 22:39:42 +0700

    New stable update

--- apicurio-registry/install.sh
+++ apicurio-registry/install.sh
@@ -5,7 +5,7 @@ APICURIO_REGISTRY_HOME=$GRAFYSI_HOME/k8s-infra/apicurio-registry
 
 NAMESPACE=apicurio
 
-kubetl get ns apicurio || kubectl create ns $NAMESPACE
+kubectl get ns apicurio || kubectl create ns $NAMESPACE
 
 cat $APICURIO_REGISTRY_HOME/resource/manifest-stable.yml |
 sed "s/apicurio-registry-operator-namespace/$NAMESPACE/g" | kubectl apply -f - -n $NAMESPACE
